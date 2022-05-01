ac584093e5580ebbe36a7fb357e86340cf3aaf31
alonso.anavalon <alonso.anavalon@xintec.cl>
2022-05-01 00:59:33 -0400

    [Suscriber] - [FEAT] - listen message listening in main

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { LoggerService } from './logger/logger.service';
+import { SuscriberModule } from './suscriber/suscriber.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    SuscriberModule,
   ],
   controllers: [],
   providers: [LoggerService],
